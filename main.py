e50c51f4f296020194663b66ec96e064aaee4b86
tarun161 <tarunsrinivas1616@gmail.com>
2026-05-15 15:13:52 +0530

    Task

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ import asyncio
 async def couroutine_fun(name : str) -> str:
     print("Entered couroutine_fun")
     await asyncio.sleep(0.1);
-    print(f"Finished couroutine_fun for {name}")
+    return f"Finished couroutine_fun for {name}"
 
 async def main() :
-    couroutine_obj = couroutine_fun("tarun")
-    print(couroutine_obj)
-    result = await couroutine_obj
-    print(result)
+    # couroutine_obj = couroutine_fun("tarun")
+    # print(couroutine_obj)
+    # result = await couroutine_obj
+    # print(result)
+    task1 = asyncio.create_task(couroutine_fun("tarun"))
+    print("task1")
+    task2 = asyncio.create_task(couroutine_fun("tarun2"))
+    print("task2")
+    task_result = await task1
+    print(task_result)
+    task_result = await task2
+    print(task_result)
 
 if __name__ == "__main__":
     asyncio.run(main())
